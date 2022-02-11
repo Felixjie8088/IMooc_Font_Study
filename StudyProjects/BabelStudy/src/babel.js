ecdd85f08a2d238221da3cb986666086ab1de4c9
FelixJie <992633726@qq.com>
2022-02-12 00:48:35 +0800

    2022-02-12:Babel的使用（Babel的安装、配置、转换案例）

---
+++ StudyProjects/BabelStudy/src/babel.js
@@ -0,0 +1,7 @@
+let name = "Felix";
+const age = 25;
+const add = (x, y) => x + y;
+new Promise((resolve, reject) => { resolve("success"); });
+Array.from([1, 2]);
+class Person { }
+import "./index.js";
