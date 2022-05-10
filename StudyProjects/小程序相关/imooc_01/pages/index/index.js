69f1eb35712bc3134e1708defc319bffef67b908
FelixJie <992633726@qq.com>
2022-05-10 18:32:28 +0800

    20220510：小程序相关-imooc_01【4.0】（header部分完成开发、导航部分完成开发、轮播部分90%）

--- StudyProjects/小程序相关/imooc_01/pages/index/index.js
+++ StudyProjects/小程序相关/imooc_01/pages/index/index.js
@@ -2,4 +2,26 @@
 // 获取应用实例
 // const app = getApp()
 
-Page({})
+Page({
+  data: {
+    showIcon: true,
+    "swiperList": [
+      {
+        "imgUrl": "https://imooc-micro-app.oss-cn-beijing.aliyuncs.com/TS.jpeg"
+      },
+      {
+        "imgUrl": "https://imooc-micro-app.oss-cn-beijing.aliyuncs.com/css.jpeg"
+      },
+      {
+        "imgUrl": "https://imooc-micro-app.oss-cn-beijing.aliyuncs.com/framework.jpeg"
+      }
+    ]
+  },
+  // headerinput框输入的时候触发的事件（将输入框中的icon隐藏）
+  handleInputChange(event) {
+    let inputVal = event.detail.value;
+    this.setData({
+      showIcon: inputVal ? false : true
+    })
+  }
+})
