8083c631b4c5affcba0f9e483100aaf1d733a3e8
FelixJie <992633726@qq.com>
2022-06-07 16:41:09 +0800

    20220607：Vue相关【12.0】CompositionAPI中使用Vuex、axios发送ajax请求

--- StudyProjects/Vue相关/vue-demo1/src/store/index.js
+++ StudyProjects/Vue相关/vue-demo1/src/store/index.js
@@ -1,26 +1,32 @@
 import {createStore} from 'vuex'
+import axios from 'axios'
 // Vuex 数据管理框架
 // Vuex 创建了一个全局唯一的仓库，用来存放全局的数据
 export default createStore({
-  state: {
-    name: 'felix'
-  },
-  getters: {},
-  mutations: {
-    // 第四步，对应的mutation被执行
-    change() {
-      // 第五步，在这里修改state里面的数据
-      console.log('mutation-change');
-      this.state.name = 'ying';
-    }
-  },
-  actions: {
-    // 第二步，store感知到你触发了一个叫做changeName的action，执行changeName方法
-    changeName() {
-      // 第三步，提交一个commit，触发一个mutation
-      console.log('action-change');
-      this.commit('change');
-    }
-  },
-  modules: {}
+	state: {
+		name: 'felix'
+	},
+	getters: {},
+	mutations: {
+		// 第四步，对应的mutation被执行
+		change(state, msg) {
+			// 第五步，在这里修改state里面的数据
+			console.log('mutation-change');
+			state.name = msg;
+		}
+	},
+	actions: {
+		// 第二步，store感知到你触发了一个叫做changeName的action，执行changeName方法
+		changeName(store) {
+			// 第三步，提交一个commit，触发一个mutation
+			console.log('action-change');
+			axios.post('https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/user/register').then((res) => {
+				console.log(res);
+				const {message} = res.data;
+				store.commit('change', message);
+			});
+			
+		}
+	},
+	modules: {}
 })
