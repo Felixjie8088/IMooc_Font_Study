8a68be3c9de9fd481fbcee4490dc34fec713651b
FelixJie <992633726@qq.com>
2022-06-18 23:01:40 +0800

    20220618：Vue相关-仿京东到家项目【3.0】首页开发初步完成，部分图片缺失未显示，组件未拆分

--- StudyProjects/Vue相关/vue-jingdong/src/App.vue
+++ StudyProjects/Vue相关/vue-jingdong/src/App.vue
@@ -2,15 +2,93 @@
   <!-- 内容区域 -->
   <div class="wrapper">
     <!-- 顶部位置定位 -->
-    <div class="position" id="position">
+    <div class="position center" id="position">
       <i class="iconfont icon-ditu_dingwei_o"></i>
       <span class="position__text">北京理工大学国防科技园2号楼10层</span>
       <i class="iconfont icon-lingdang"></i>
     </div>
     <!-- 搜索框 -->
-    <div class="search">
+    <div class="search center">
       <i class="iconfont icon-search"></i>
-      <input type="text" class="searchbox" id="searchcontent" placeholder="山姆会员商店优惠商品">
+      <input type="text" class="search__box" id="searchcontent" placeholder="山姆会员商店优惠商品">
+    </div>
+    <!-- swiper 轮播图 -->
+    <div class="swiper center">
+    </div>
+    <!-- 图标菜单列表 -->
+    <nav class="nav__list center">
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/超市.png" alt="">
+        <span class="nav__item__text">超市便利</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/菜市场.png" alt="">
+        <span class="nav__item__text">菜市场</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/水果店.png" alt="">
+        <span class="nav__item__text">水果店</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/鲜花.png" alt="">
+        <span class="nav__item__text">鲜花绿植</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/医药健康.png" alt="">
+        <span class="nav__item__text">医药健康</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/家居.png" alt="">
+        <span class="nav__item__text">家居时尚</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/蛋糕.png" alt="">
+        <span class="nav__item__text">烘焙蛋糕</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/签到.png" alt="">
+        <span class="nav__item__text">签到</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/大牌免运.png" alt="">
+        <span class="nav__item__text">大牌免运</span>
+      </div>
+      <div class="nav__item">
+        <img class="nav__item__icon" src="./style/assets/红包.png" alt="">
+        <span class="nav__item__text">红包套餐</span>
+      </div>
+    </nav>
+    <!-- 分隔线 -->
+    <div class="split__line"></div>
+    <!-- 附近店铺 -->
+    <div class="nearby-shops center">
+      <p class="title">附近店铺</p>
+      <ul class="store-list">
+        <li class="store-item">
+          <img src="" alt="" class="store-item-icon">
+          <div class="store-item-info">
+            <p class="info-store-name">沃尔玛</p>
+            <p class="info-store-cost">
+              <span class="info-store-cost-text">月售1万+</span>
+              <span class="info-store-cost-text">起送￥0</span>
+              <span class="info-store-cost-text">基础运费￥5</span>
+            </p>
+            <p class="info-store-promotions">VIP尊享满89元减4元运费券（每月3张）</p>
+          </div>
+        </li>
+        <li class="store-item">
+          <img src="" alt="" class="store-item-icon">
+          <div class="store-item-info">
+            <p class="info-store-name">山姆会员商店</p>
+            <p class="info-store-cost">
+              <span class="info-store-cost-text">月售2千+</span>
+              <span class="info-store-cost-text">起送￥0</span>
+              <span class="info-store-cost-text">基础运费￥5</span>
+            </p>
+            <p class="info-store-promotions">联合利华洗护满10减5</p>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
   <!-- 底部菜单栏 -->
@@ -35,13 +113,18 @@
 </template>
 
 <style lang="scss">
+  // 居中样式
+  .center {
+    margin: 0 .18rem;
+  }
+
   .wrapper {
     position: absolute;
     left: 0;
     top: 0;
     right: 0;
     bottom: .51rem;
-    padding: 0 .18rem;
+    overflow: hidden;
 
     // 位置定位
     .position {
@@ -79,16 +162,15 @@
       position: relative;
       display: flex;
       width: 3.39rem;
-      height: .16rem;
-      padding: .08rem 0;
+      height: .32rem;
       line-height: .32rem;
 
-      .searchbox {
+      &__box {
         padding: 0;
         width: 100%;
         height: .32rem;
         background: #f5f5f5;
-        border: 1px solid rgba(0, 0, 0, 0);
+        border: 0 solid rgba(0, 0, 0, 0);
         border-radius: .16rem;
         outline: none;
 
@@ -105,6 +187,108 @@
         color: #B7B7B7;
         font-size: .16rem;
         left: .16rem;
+      }
+    }
+
+    // 轮播图
+    .swiper {
+      background: blue;
+      margin-top: .12rem;
+      height: .86rem;
+    }
+
+    // 图标列表
+    .nav__list {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: center;
+      padding: .16rem .08rem 0 .08rem;
+
+      .nav__item {
+        display: flex;
+        width: 20%;
+        min-width: .48rem;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: .16rem;
+
+        &__icon {
+          width: 0.4rem;
+          height: 0.4rem;
+          margin-bottom: .06rem;
+        }
+
+        &__text {
+          color: $content-fontcolor;
+        }
+      }
+    }
+
+    // 分隔线
+    .split__line {
+      height: .10rem;
+      background: #F1F1F1;
+    }
+
+    // 附近店铺
+    .nearby-shops {
+      .title {
+        font-size: .18rem;
+        margin: .16rem 0 .14rem 0;
+        color: $content-fontcolor;
+      }
+
+      // 店铺列表
+      .store-list {
+        list-style: none;
+        padding: 0;
+
+        .store-item {
+          display: flex;
+          margin-bottom: .12rem;
+
+          // 店铺logo
+          &-icon {
+            background: #B7B7B7;
+            width: 0.56rem;
+            height: 0.56rem;
+            margin-right: .16rem;
+          }
+
+          // 店铺信息
+          &-info {
+            flex-grow: 1;
+            border-bottom: .01rem solid #F1F1F1;
+
+            // 店铺名称
+            .info-store-name {
+              height: 0.22rem;
+              font-size: .16rem;
+              color: $content-fontcolor;
+            }
+
+            // 店铺基础费用信息
+            .info-store-cost {
+              height: 0.18rem;
+              margin: .08rem 0;
+
+              &-text {
+                font-size: .13rem;
+                color: $content-fontcolor;
+                margin-right: .16rem;
+              }
+            }
+
+            // 店铺优惠
+            .info-store-promotions {
+              margin-bottom: .12rem;
+              height: 0.18rem;
+              font-size: .13rem;
+              color: #E93B3B;
+            }
+          }
+        }
       }
     }
   }
@@ -131,6 +315,7 @@
 
     &__item {
       display: flex;
+      min-width: .40rem;
       flex-direction: column;
       align-items: center;
       justify-content: space-around;
